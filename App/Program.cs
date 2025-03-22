5a95a4a2d92d87e16b472af7d568d1d80131701f
Daniel Trubač <40642559+dant02@users.noreply.github.com>
2025-03-22 07:42:14 +0100

    add basic console app

---
+++ App/Program.cs
@@ -0,0 +1,15 @@
+﻿//-----------------------------------------------------------------------
+//  <author>https://github.com/dant02</author>
+//  <date>2025-03-22</date>
+//-----------------------------------------------------------------------
+
+namespace App
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
